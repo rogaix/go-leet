2ee55adbd9b7a832338f5aac60c32fdac2d26d18
rogaix <roger.paland@gmail.com>
2025-04-22 17:58:34 +0200

    Add palindrome number solution in Go
    Implemented the `isPalindrome` function to check if an integer is a palindrome. This is a simple and efficient solution for the LeetCode problem "Palindrome Number".

---
+++ easy/palindrom_number/palindrom.go
@@ -0,0 +1,30 @@
+package palindrom_number
+
+// https://leetcode.com/problems/palindrome-number/
+
+// isPalindrom 1ms answer
+func isPalindrome(x int) bool {
+	if x == -1 {
+		return false
+	}
+
+	last := x % 10
+	if last == x {
+		return true
+	}
+
+	var rev int
+	rest := x
+	for {
+		rev = rev*10 + last
+		if rev == x {
+			return true
+		}
+
+		rest = rest / 10
+		last = rest % 10
+		if rest <= 0 {
+			return false
+		}
+	}
+}

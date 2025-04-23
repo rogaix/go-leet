9227f82abde7aa14cdc1106c7a484b1badc97f4e
rogaix <roger.paland@gmail.com>
2025-04-23 09:09:36 +0200

    - No infinite loop - clear termination condition: x > reversed. - Only reverse half the number to avoid overflows and save time. - Immediate exclusion of impossible cases - negative numbers or 10, 100 etc.

--- easy/palindrom_number/palindrom.go
+++ easy/palindrom_number/palindrom.go
@@ -3,6 +3,20 @@ package palindrom_number
 // https://leetcode.com/problems/palindrome-number/
 
 func isPalindrome(x int) bool {
+	if x < 0 || (x%10 == 0 && x != 0) {
+		return false
+	}
+
+	reversed := 0
+	for x > reversed {
+		reversed = reversed*10 + x%10
+		x /= 10
+	}
+
+	return x == reversed || x == reversed/10
+}
+
+func isPalindromeOld(x int) bool {
 	if x == -1 {
 		return false
 	}
